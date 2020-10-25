0f79274ceda1827d1cb019e9200bdd7d9f39776d
Novik Vlad <Vorobushek3333@mail.ru>
2020-10-25 23:35:49 +0300

    first commit

---
+++ src/main/java/com/example/Finaccounting/FinAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Finaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Finaccounting/FinAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Finaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
